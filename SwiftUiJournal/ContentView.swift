f929ba415ef086124c5402a5ae5c279a628ac23a
jonatan <jonatan.gron.code@hotmail.com>
2023-01-08 23:37:08 +0100

    content

--- SwiftUiJournal/ContentView.swift
+++ SwiftUiJournal/ContentView.swift
@@ -11,6 +11,14 @@ struct ContentView: View {
     // dagbok med lista av anteckningar
     var journal = [JournalEntry]()
     
+    init() {
+        journal.append(JournalEntry(content: "åt mat"))
+        journal.append(JournalEntry(content: "sov"))
+        journal.append(JournalEntry(content: "Programmerade"))
+        journal.append(JournalEntry(content: "badade"))
+
+    }
+    
     var body: some View {
         List(journal) { entry in
             Text(entry.content)
